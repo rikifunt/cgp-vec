6aaeab360131a54e4a22269f4f533f8c2d160067
rikifun-t <rikifunt@gmail.com>
2022-04-12 14:01:46 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='tcgp',
+    name='pcgp',
     version='0.0.1',
-    packages=['tcgp'],
+    packages=['pcgp'],
     install_requires=[
         'torch',
       ],
